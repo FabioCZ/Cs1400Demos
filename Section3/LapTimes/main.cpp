a111e29514850d706eac053c766f2cde6e411ea2
Fabio Gottlicher <g.fabio.cz@gmail.com>
2016-11-14 08:55:34 -0700

    added stuff for arrays lap times demo

---
+++ Section3/LapTimes/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <fstream>
+
+const int recordCount = 14;
+
+int main() {
+    //read in from times.txt into an array
+    //then, report best, worst and average time
+    return 0;
+}
