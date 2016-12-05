0ccfa4450d722585f5c1738132e583bc7f7e681a
Fabio <g.fabio.cz@gmail.com>
2016-12-05 10:15:51 -0700

    added exam review

---
+++ Section3/untitled1/cmake-build-debug/CMakeFiles/Exam3Review.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Exam3Review.dir/main.cpp.obj"
+  "Exam3Review.pdb"
+  "Exam3Review.exe"
+  "Exam3Review.exe.manifest"
+  "libExam3Review.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Exam3Review.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
